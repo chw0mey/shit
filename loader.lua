e1f79e2ed59468ad86ed43139eb27431402d50e9
chw0mey <108242701+chw0mey@users.noreply.github.com>
2022-08-17 13:37:07 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,3 +1,5 @@
 if tostring(game.PlaceId) == '9498006165' then
     loadstring(game:HttpGet('https://raw.githubusercontent.com/chw0mey/shit/main/nig.lua'))('chwome better than lisp')
+else then
+    createnotification('ERR: Game Not Supported')
 end
