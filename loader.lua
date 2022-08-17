7c57c1d0dee1e494b8794104abb659f0a4b8a3f8
chw0mey <108242701+chw0mey@users.noreply.github.com>
2022-08-17 13:53:43 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,8 +1,3 @@
 if tostring(game.PlaceId) == '9498006165' then
     loadstring(game:HttpGet('https://raw.githubusercontent.com/chw0mey/shit/main/nig.lua'))('chwome better than lisp')
-else then
-    game.StarterGui:SetCore("SendNotification",  {
-        Title = "Game Invalid!";
-        Text = "Please Check The Discord For All Valid Games!";
-    })
 end
